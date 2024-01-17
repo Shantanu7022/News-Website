85774f028560ae9e39d3db397718e0a47936e7ea
Shantanu Gayen <shantanugayen200207@gmail.com>
2024-01-17 20:51:34 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const API_KEY = "4ea8e2d58ed04302a1f66dde621a7342";
-const url = "https://newsapi.org/v2/everything?q=";
+let API_KEY = "4ea8e2d58ed04302a1f66dde621a7342";
+let cardData = document.querySelector(".cardData")
 
-async function fetchData(query){
-    const result = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-    const data = await result.json();
-    console.log(data);
+const getData = async() =>{
+    let res =await fetch(`https://newsapi.org/v2/everything?q=virat&apiKey=${API_KEY}`);
+    let jsonData =await res.json();
+    console.log(jsonData.articles[0]);
+
+    jsonData.articles.forEach(function(article){
+        console.log(article);
+
+    let divs = document.createElement("div");
+    divs.classList.add("card");
+    cardData.appendChild(divs);
+
+    divs.innerHTML = `
+    <img src="${article.urlToImage}" alt="">
+    <h3>${article.title}</h3>
+    <p>${article.description}</p>
+
+    `
+    })
+
+    
 }
 
-fetchData("all")
-
-let mobilemenu = document.querySelector(".mobile")
-let menuBtn = document.querySelector(".menuBtn")
-let menuBtnDisplay = true;
-
-menuBtn.addEventListener("click",()=>{
-    mobilemenu.classList.toggle("hidden")
-})
-
+getData()
